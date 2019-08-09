97deb2f19dc9c954906c4132292b65853c30cd49
raven02100 <raven02100@gmail.com>
2019-08-09 22:04:54 +0700

    Heroku commit

--- client.js
+++ client.js
@@ -4,3 +4,11 @@ let a = 0;
             a += 1;
             document.getElementById("abc").innerText = a;
         }
+
+        function callServer() {
+            window.fetch("http://localhost:8000", {method: "GET"}) //thực hiện lệnh get trên đường link kia//
+                .then(function(response) {return response.json();
+                })
+                .then(function(data) {document.getElementsByClassName("search-box")[0].value = data;
+                });
+        }
--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 app.use(cors({ origin: "*" }));
-app.get("/", (req, res) => res.status(200).json("Hello world"));
+app.get("/", function(req, res) {
+  setTimeout(function() {
+    res.status(200).json("Hello world");
+  }, 1000);
+});
 
 app.get("/abc/:ib", (req, res) => {
   res.status(200).json(req.params);
 });
 
 app.listen(8000)
+// promise.then(function(response) {} - cái function bên trong là 1 câu lệnh chỉ được thực hiện khi promise đã có kết quả //
+// local có thể đc coi là máy tính đang chứa server chủ, 8000 là port //
+//app.get thì "get" ở đây là 1 câu lệnh chỉ dùng để lấy dữ liệu //
+//setTimeout là đặt thời gian hết hạn, mục đích là thực hiện 1 function sau 1 khoảng thời gian hết hạn, có 2 biến - 1 là function, 2 là thời gian hết hạn (đơn vị là millisecond)//
